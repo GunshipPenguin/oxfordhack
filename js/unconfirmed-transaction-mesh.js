b17d06c0369966080be25df749e21ed2877d12fd
Rhys Rustad-Elliott <me@rhysre.net>
2018-11-24 19:35:14 +0000

    Add block movement

--- js/unconfirmed-transaction-mesh.js
+++ js/unconfirmed-transaction-mesh.js
@@ -1,9 +1,24 @@
 var THREE = require('three');
 
 class UnconfirmedTransactionMesh extends THREE.Mesh {
-    constructor() {
+    constructor(txInfo) {
         super(new THREE.BoxBufferGeometry(0.1, 0.1, 0.1),
             new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff}));
+
+        this.txInfo = txInfo;
+
+        this.velocityX = Math.random() * 0.01 - 0.05;
+        this.velocityY = Math.random() * 0.01 - 0.05;
+        this.velocityZ = Math.random() * 0.01 - 0.05;
+    }
+
+    getEstimatedAmount() {
+        var smallest = Infinity;
+        this.txInfo.x.out.forEach(output => {
+            smallest = Math.min(smallest, output.value);
+        });
+
+        return smallest;
     }
 }
 
