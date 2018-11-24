3687cb3856bb144ba6f739efa14e6abf2cb2cd75
muke101 <muke101@protonmail.com>
2018-11-24 20:10:10 +0000

    added block functoin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var THREE = require('three');
 var WEBVR = require('./web-vr');
 var Room = require('./room');
+var WEBVR = require('./web-vr');
+var Room = require('./room');
 var blockchainApi = require('./blockchain-api');
 
 var container;
@@ -19,6 +21,256 @@ function init() {
     document.body.appendChild(container);
 
     var info = document.createElement('div');
+var WEBVR = require('./web-vr');
+var Room = require('./room');
+var blockchainApi = require('./blockchain-api');
+
+var container;
+var camera, scene, renderer;
+var clock;
+
+var room;
+var crosshair;
+
+init();
+animate();
+
+function init() {
+    clock = new THREE.Clock();
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    var info = document.createElement('div');
+    info.style.position = 'absolute';
+    info.style.top = '10px';
+    info.style.width = '100%';
+    info.style.textAlign = 'center';
+    info.innerHTML = '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> webgl - interactive cubes';
+    container.appendChild(info);
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x505050);
+
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10);
+    scene.add(camera);
+
+    crosshair = new THREE.Mesh(
+        new THREE.RingBufferGeometry(0.02, 0.04, 32),
+        new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            opacity: 0.5,
+            transparent: true
+        })
+    );
+    crosshair.position.z = -2;
+    camera.add(crosshair);
+
+    room = new Room(4);
+
+    room.position.y = 3;
+    scene.add(room);
+
+    blockchainApi.subscribeToTransactions(t => {
+        
+        room.addUnconfirmedTransaction(t);
+    });
+
+    scene.add(new THREE.HemisphereLight(0x606060, 0x404040));
+
+    var light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(1, 1, 1).normalize();
+    scene.add(light);
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.vr.enabled = true;
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onWindowResize, false);
+
+    window.addEventListener('vrdisplaypointerrestricted', onPointerRestricted, false);
+    window.addEventListener('vrdisplaypointerunrestricted', onPointerUnrestricted, false);
+
+    document.body.appendChild(WEBVR.createButton(renderer));
+}
+
+function onPointerRestricted() {
+    var pointerLockElement = renderer.domElement;
+    if (pointerLockElement && typeof (pointerLockElement.requestPointerLock) === 'function') {
+        pointerLockElement.requestPointerLock();
+    }
+}
+
+function onPointerUnrestricted() {
+    var currentPointerLockElement = document.pointerLockElement;
+    var expectedPointerLockElement = renderer.domElement
+    info.style.position = 'absolute';
+    info.style.top = '10px';
+    info.style.width = '100%';
+    info.style.textAlign = 'center';
+    info.innerHTML = '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> webgl - interactive cubes';
+    container.appendChild(info);
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x505050);
+
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10);
+    scene.add(camera);
+
+    crosshair = new THREE.Mesh(
+        new THREE.RingBufferGeometry(0.02, 0.04, 32),
+        new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            opacity: 0.5,
+            transparent: true
+        })
+    );
+    crosshair.position.z = -2;
+    camera.add(crosshair);
+
+    room = new Room(4);
+
+    room.position.y = 3;
+    scene.add(room);
+
+    blockchainApi.subscribeToTransactions(t => {
+        console.log(t);
+        room.addUnconfirmedTransaction(t);
+    });
+
+    scene.add(new THREE.HemisphereLight(0x606060, 0x404040));
+
+    var light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(1, 1, 1).normalize();
+    scene.add(light);
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.vr.enabled = true;
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onWindowResize, false);
+
+    window.addEventListener('vrdisplaypointerrestricted', onPointerRestricted, false);
+    window.addEventListener('vrdisplaypointerunrestricted', onPointerUnrestricted, false);
+
+    document.body.appendChild(WEBVR.createButton(renderer));
+}
+
+function onPointerRestricted() {
+    var pointerLockElement = renderer.domElement;
+    if (pointerLockElement && typeof (pointerLockElement.requestPointerLock) === 'function') {
+        pointerLockElement.requestPointerLock();
+    }
+}
+
+function onPointerUnrestricted() {
+    var currentPointerLockElement = document.pointerLockElement;
+    var expectedPointerLockElement = renderer.domElement
+var blockchainApi = require('./blockchain-api');
+
+var container;
+var camera, scene, renderer;
+var clock;
+
+var room;
+var crosshair;
+
+init();
+animate();
+
+function init() {
+    clock = new THREE.Clock();
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    var info = document.createElement('div');
+var WEBVR = require('./web-vr');
+var Room = require('./room');
+var blockchainApi = require('./blockchain-api');
+
+var container;
+var camera, scene, renderer;
+var clock;
+
+var room;
+var crosshair;
+
+init();
+animate();
+
+function init() {
+    clock = new THREE.Clock();
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    var info = document.createElement('div');
+    info.style.position = 'absolute';
+    info.style.top = '10px';
+    info.style.width = '100%';
+    info.style.textAlign = 'center';
+    info.innerHTML = '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> webgl - interactive cubes';
+    container.appendChild(info);
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x505050);
+
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10);
+    scene.add(camera);
+
+    crosshair = new THREE.Mesh(
+        new THREE.RingBufferGeometry(0.02, 0.04, 32),
+        new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            opacity: 0.5,
+            transparent: true
+        })
+    );
+    crosshair.position.z = -2;
+    camera.add(crosshair);
+
+    room = new Room(4);
+
+    room.position.y = 3;
+    scene.add(room);
+
+    blockchainApi.subscribeToTransactions(t => {
+        console.log(t);
+        room.addUnconfirmedTransaction(t);
+    });
+
+    scene.add(new THREE.HemisphereLight(0x606060, 0x404040));
+
+    var light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(1, 1, 1).normalize();
+    scene.add(light);
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.vr.enabled = true;
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onWindowResize, false);
+
+    window.addEventListener('vrdisplaypointerrestricted', onPointerRestricted, false);
+    window.addEventListener('vrdisplaypointerunrestricted', onPointerUnrestricted, false);
+
+    document.body.appendChild(WEBVR.createButton(renderer));
+}
+
+function onPointerRestricted() {
+    var pointerLockElement = renderer.domElement;
+    if (pointerLockElement && typeof (pointerLockElement.requestPointerLock) === 'function') {
+        pointerLockElement.requestPointerLock();
+    }
+}
+
+function onPointerUnrestricted() {
+    var currentPointerLockElement = document.pointerLockElement;
+    var expectedPointerLockElement = renderer.domElement
     info.style.position = 'absolute';
     info.style.top = '10px';
     info.style.width = '100%';
@@ -117,13 +369,22 @@ function buildBlock(transactionIDs)
 	}
 }
 
-// function findBox(boxID){
+function findBox(boxID){
+	var boxes = room.children
 
-// 	for (box in boxes) 
-// 		if (box.id = boxID)
-// 			 return box
+	for (var i = 0; i < boxes.length; ++i)
+	{
+		if (boxes[i].txInfo.x.tx_index == boxID)
+		{
+			 return boxes[i]
+		}
+	}
+}
 
-// }
+function moveBox(box)
+{
+	
+}
 
 //-------------------------------
 
