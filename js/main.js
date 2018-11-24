ab05d57c503873a1d03747bd85a731d869609a2e
muke101 <muke101@protonmail.com>
2018-11-24 22:40:58 +0000

    catch up

--- js/main.js
+++ js/main.js
@@ -1,28 +1,6 @@
 var THREE = require('three');
 var WEBVR = require('./web-vr');
 var Room = require('./room');
-var WEBVR = require('./web-vr');
-var Room = require('./room');
-var blockchainApi = require('./blockchain-api');
-
-var container;
-var camera, scene, renderer;
-var clock;
-
-var room;
-var crosshair;
-
-init();
-animate();
-
-function init() {
-    clock = new THREE.Clock();
-    container = document.createElement('div');
-    document.body.appendChild(container);
-
-    var info = document.createElement('div');
-var WEBVR = require('./web-vr');
-var Room = require('./room');
 var blockchainApi = require('./blockchain-api');
 
 var container;
@@ -70,73 +48,10 @@ function init() {
     room.position.y = 3;
     scene.add(room);
 
-    blockchainApi.subscribeToTransactions(t => {
-        transaction = JSON.parse(e.data).x
-        transactions.push(transaction.tx_index)
-        room.addUnconfirmedTransaction(t);
-    });
-
-    scene.add(new THREE.HemisphereLight(0x606060, 0x404040));
-
-    var light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(1, 1, 1).normalize();
-    scene.add(light);
-
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.vr.enabled = true;
-    container.appendChild(renderer.domElement);
-f
-    window.addEventListener('resize', onWindowResize, false);
-
-    window.addEventListener('vrdisplaypointerrestricted', onPointerRestricted, false);
-    window.addEventListener('vrdisplaypointerunrestricted', onPointerUnrestricted, false);
-
-    document.body.appendChild(WEBVR.createButton(renderer));
-}
-
-function onPointerRestricted() {
-    var pointerLockElement = renderer.domElement;
-    if (pointerLockElement && typeof (pointerLockElement.requestPointerLock) === 'function') {
-        pointerLockElement.requestPointerLock();
-    }
-}
-
-function onPointerUnrestricted() {
-    var currentPointerLockElement = document.pointerLockElement;
-    var expectedPointerLockElement = renderer.domElement
-    info.style.position = 'absolute';
-    info.style.top = '10px';
-    info.style.width = '100%';
-    info.style.textAlign = 'center';
-    info.innerHTML = '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> webgl - interactive cubes';
-    container.appendChild(info);
-
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x505050);
-
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10);
-    scene.add(camera);
-
-    crosshair = new THREE.Mesh(
-        new THREE.RingBufferGeometry(0.02, 0.04, 32),
-        new THREE.MeshBasicMaterial({
-            color: 0xffffff,
-            opacity: 0.5,
-            transparent: true
-        })
-    );
-    crosshair.position.z = -2;
-    camera.add(crosshair);
-
-    room = new Room(4);
-
-    room.position.y = 3;
-    scene.add(room);
+    transactions = []
 
     blockchainApi.subscribeToTransactions(t => {
-        console.log(t);
+        transactions.push(JSON.parse(t.data).x.tx_index)
         room.addUnconfirmedTransaction(t);
     });
 
@@ -169,7 +84,11 @@ function onPointerRestricted() {
 
 function onPointerUnrestricted() {
     var currentPointerLockElement = document.pointerLockElement;
-    var expectedPointerLockElement = renderer.domElement
+    var expectedPointerLockElement = renderer.domElement;
+    if (currentPointerLockElement && currentPointerLockElement === expectedPointerLockElement && typeof (document.exitPointerLock) === 'function') {
+        document.exitPointerLock();
+    }
+}
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -189,34 +108,33 @@ function render() {
     renderer.render(scene, camera);
 }
 
+
 function buildBlock(transactionIDs)
 {
-	for (var i = 0; i < transactionIDs.length; i++)
-	{
-		findBox(transactionsIDs[i])
-      .then(moveBox(box))
-      .catch(console.error)
-	}
+    for (var i = 0; i < transactionIDs.length; i++)
+    {
+        findBox(transactionIDs[i])
+         .then(moveBox(box))
+         .catch(console.error)
+    }
 
-  room.addConfirmedTransaction();
+    room.addConfirmedTransaction();
 }
 
 function findBox(boxID){
-	var boxes = room.children
+    var boxes = room.children
 
-  const promise = new Promise ((resolve, reject) => {
-    for (var i = 0; i < boxes.length; ++i)
-    {
-      if (boxes[i].txInfo.x.tx_index == boxID)
-      {
-         resolve(boxes[i])
-      }
+    const promise = new Promise ((resolve, reject) => {
+        for (var i = 0; i < boxes.length; ++i)
+        {
+            if (boxes[i].txInfo.x.tx_index == boxID)
+            {
+                resolve(boxes[i])
+            }
+        }
+        reject('box not found')
     }
-
-    reject('box not found')
-  });
-
-  return promise
+    return promise
 }
 
 function moveBox(box)
@@ -226,7 +144,7 @@ function moveBox(box)
 }
 
 function removeBlock()
-
+{
 	block = JSON.parse(e.data).x
 	for (var i=0; i<block.txIndexes.length; ++i)
 	{
